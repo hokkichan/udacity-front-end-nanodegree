c3dda33c259eaacd23207c44e5e4e96598c31915
hokkichan <57009866+hokkichan@users.noreply.github.com>
2020-08-06 01:13:25 +0800

    Install packages

---
+++ api-sandbox/asyncPostGet.js
@@ -0,0 +1,32 @@
+// Async POST
+const postData = async ( url = '', data = {})=>{
+
+    const response = await fetch(url, {
+    method: 'POST', 
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
+
+    try {
+      const newData = await response.json();
+      return newData
+    }catch(error) {
+    console.log("error", error);
+    }
+}
+
+// TODO-Async GET
+const retrieveData = async (url='') =>{ 
+  const request = await fetch(url);
+  try {
+  // Transform into JSON
+  const allData = await request.json()
+  }
+  catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+}
